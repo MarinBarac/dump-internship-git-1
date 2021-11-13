bd71d784b96fd55f1bd5adcf98045c9fbd55cb82
Marin Barac <mbarac00@fesb.hr>
2021-11-13 15:05:34 +0100

    Marin je napravio prvi potez

--- tic_tac_toe/Program.cs
+++ tic_tac_toe/Program.cs
@@ -9,7 +9,7 @@ namespace tic_tac_toe
             Console.WriteLine("Marin ima x pobjeda i y poraza!");
             Console.WriteLine("Boze ima z pobjeda i n poraza!");
 
-            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   | x ");
             Console.WriteLine("   |   |   ");
             Console.WriteLine("   |   |   ");
         }
